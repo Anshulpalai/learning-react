7ffdb30caa54deb85779c0f6f7e7538bf2a23ed4
Anshul Palai <palaianshul@gmail.com>
2024-05-11 23:11:55 +0530

    Adding pages

---
+++ 13_Mega_Blog_Project/src/pages/AddPost.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, PostForm } from '../components';
+Container
+
+function AddPost() {
+    return (
+        <div className='py-8'>
+            <Container>
+                <PostForm />
+            </Container>
+        </div>
+    )
+}
+
+export default AddPost
